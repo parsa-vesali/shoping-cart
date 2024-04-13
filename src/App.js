d7d92bf0a50deaa65b152f46a49b96d1d1353f9b
Parsa Vesali <parsavesali84@gmail.com>
2024-04-13 18:59:33 +0330

    add folders

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Gi3DGlasses } from "react-icons/gi";
+
+function App() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <h3 className="text-white">تست</h3>
+        </div>
+      </nav>
+      <h1><Gi3DGlasses /></h1>
+    </div>
+  );
+}
+
+export default App;
